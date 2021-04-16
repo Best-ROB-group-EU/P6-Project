cea13fd32cfd6643cc0426b6e898fb82beb70232
Bagiukstis <jedziukas14@gmail.com>
2021-04-16 11:34:27 +0200

    Adding a script for generating, cropping and plotting pointclouds

---
+++ RealSense/plotter.m
@@ -0,0 +1,13 @@
+
+pt1Cloud2 = pcread('/home/vdr/Desktop/RealSense/great_pointcloud_path/2021-04-16 10:56:29.859247.ply');
+
+
+pcshow(pt1Cloud2);
+
+
+
+
+
+%figure
+%A = imread('/home/vdr/Desktop/RealSense/test.jpg');
+%image(A)
