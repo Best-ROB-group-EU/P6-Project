c417bb40c77f8db44fe953be30d47c0b915b3c2d
frederikjchristensen <frederikjchristensen@gmail.com>
2021-04-30 09:27:03 +0200

    Adding small changes

---
+++ Handler.py
@@ -0,0 +1,8 @@
+import json
+
+def Log_image ():
+    Record = {
+        'type': 'missing joint material',
+        'location': ['1', '2', '3'],
+        'image': 'missing_24_04_2021-10_48-1.bag'
+        }
